7d8092d0c544c6699cbf5a396b0021e59a5ea37d
Marcylene Barreto <marsouza93@gmail.com>
2024-12-30 16:23:15 -0300

    initial commit

---
+++ busease-app/busease-app/busease_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  busease_appApp.swift
+//  busease-app
+//
+//  Created by Marcylene Barreto on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct busease_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
